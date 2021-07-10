9872d2a53b11d5486f0135d1cd21399727676dd2
rogercomp <rogercomp@gmail.com>
2021-07-10 19:05:43 -0300

    first commit

---
+++ src/main/java/com/br/tess4tj/app/LerImagem.java
@@ -0,0 +1,23 @@
+package com.br.tess4tj.app;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LerImagem {
+
+    public static void main(String[] args) throws IOException {
+        File imageFile = new File("./src/main/resources/teste.png");
+        Tesseract tess4j = new Tesseract();
+       // tess4j.setDatapath("C:/Temp/tessdata");
+        tess4j.setLanguage("por");
+        try {
+            String result = tess4j.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
